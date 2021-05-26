bdd4a8b323aebd9e9f8a2118a4871de544634154
栗山未来 <pusoxz@gmail.com>
2021-05-26 05:07:48 +0000

    Update A.sh

--- script/A.sh
+++ script/A.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 t=$(cat /etc/vim/vimrc.tiny | grep nocompatible | wc -l)
 if [ $t -eq 0 ]; then
-  echo vim
   sed -i "s/compatible/nocompatible/g" /etc/vim/vimrc.tiny
   echo 'set backspace=2' >> /etc/vim/vimrc.tiny
 fi
