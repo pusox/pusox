f8b47f6c461f4db64409e1f16d91643a8fa3d039
栗山未来 <pusoxz@gmail.com>
2021-05-31 09:20:08 +0000

    Update iptables.sh

--- script/iptables.sh
+++ script/iptables.sh
@@ -15,5 +15,5 @@ COMMIT
 /sbin/iptables-restore < /etc/iptables.up.rules
 echo '#!/bin/sh
 /sbin/iptables-restore < /etc/iptables.up.rules' > /etc/network/if-pre-up.d/iptables
-echo 'alias save-iptables=iptables-save > /etc/iptables.up.rules'
+echo "alias save-iptables='iptables-save > /etc/iptables.up.rules'" >> .bashrc
 chmod +x /etc/network/if-pre-up.d/iptables
