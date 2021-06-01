e59bebfb1fa4b7734797676ae9a7b713992a4df8
栗山未来 <debuffxb@gmail.com>
2021-06-01 17:36:18 +0800

    feat: add enbale bbr

---
+++ script/enableBBR.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+sysctl net.ipv4.tcp_available_congestion_control
+echo ======================================
+lsmod | grep bbr
