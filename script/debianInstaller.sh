619b09c9a5a05d247d4b86a9c5678747a19be42c
栗山未来 <pusoxz@gmail.com>
2021-06-29 10:42:18 +0800

    feat: change debian mirror

--- script/debianInstaller.sh
+++ script/debianInstaller.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 mkdir /boot/newinstall
-wget "http://mirrors.cloud.tencent.com/debian/dists/buster/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz" -O /boot/newinstall/initrd.gz
-wget "http://mirrors.cloud.tencent.com/debian/dists/buster/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux" -O /boot/newinstall/linux
+wget "http://ftp.debian.org/debian/dists/buster/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz" -O /boot/newinstall/initrd.gz
+wget "http://ftp.debian.org/debian/dists/buster/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux" -O /boot/newinstall/linux
 echo "menuentry 'New Install' {
 insmod part_msdos
 insmod ext2
