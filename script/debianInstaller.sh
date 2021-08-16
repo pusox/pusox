7c5e258c8957c0370a622258e4018103319c6a88
栗山未来 <pusoxz@gmail.com>
2021-08-16 07:42:30 +0000

    Update debianInstaller.sh

--- script/debianInstaller.sh
+++ script/debianInstaller.sh
@@ -7,8 +7,8 @@ elif [ $(arch) = 'x86_64' ]
 then
   arch='amd64'
 fi
-wget "http://ftp.debian.org/debian/dists/buster/main/installer-$arch/current/images/netboot/debian-installer/$arch/initrd.gz" -O /boot/newinstall/initrd.gz
-wget "http://ftp.debian.org/debian/dists/buster/main/installer-$arch/current/images/netboot/debian-installer/$arch/linux" -O /boot/newinstall/linux
+wget "http://ftp.debian.org/debian/dists/stable/main/installer-$arch/current/images/netboot/debian-installer/$arch/initrd.gz" -O /boot/newinstall/initrd.gz
+wget "http://ftp.debian.org/debian/dists/stable/main/installer-$arch/current/images/netboot/debian-installer/$arch/linux" -O /boot/newinstall/linux
 echo "menuentry 'New Install' {
 insmod part_msdos
 insmod ext2
