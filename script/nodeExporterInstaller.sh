fb252ccb8b545a2088dbcc22b3f60172dd7d79ec
栗山未来 <pusoxz@gmail.com>
2021-08-25 03:13:58 +0000

    Add new file

---
+++ script/nodeExporterInstaller.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+if [ -d "/usr/local/node_exporter" ]
+then
+  echo 'PATH: /usr/local/node_exporter exists, Exit.'
+  exit
+fi
+if [ $(arch) = 'aarch64' ]
+then
+  arch='arm64'
+elif [ $(arch) = 'x86_64' ]
+then
+  arch='amd64'
+fi
+wget "https://github.com/prometheus/node_exporter/releases/download/v1.2.2/node_exporter-1.2.2.linux-$arch.tar.gz" -O- | tar -xJvf -
+mv node_exporter* /usr/local/node_exporter
+echo '[Unit]
+Description=node_export
+Documentation=https://github.com/prometheus/node_exporter
+After=network.target
+ 
+[Service]
+Type=simple
+ExecStart=/usr/local/node_exporter/node_exporter --web.listen-address=":9200"
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/node_exporter.service
+systemctl enable node_exporter
+systemctl start node_exporter
